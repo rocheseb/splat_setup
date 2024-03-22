52541f6a1e728e5ee25b30bf582bd2068c41a9ff
Sebastien Roche <sebastien.roche@mail.utoronto.ca>
2024-03-22 17:14:52 -0400

    update setup.py for better version handling

--- setup.py
+++ setup.py
@@ -1,8 +1,40 @@
-from setuptools import setup
+import os
+import re
+import subprocess
+from setuptools import setup, find_packages
+
+
+_mydir = os.path.dirname(__file__)
+
+# parse version number from splatsetup/__init__.py
+version_re = r"^__version__ = ['\"]([^'\"]*)['\"]"
+with open(os.path.join(_mydir, "splatsetup", "__init__.py")) as f:
+    content = f.read()
+re_search = re.search(version_re, content, re.M)
+if re_search:
+    version_str = re_search.group(1)
+else:
+    raise RuntimeError("Could not parse version string from __init__.py")
+
+# add the git hash to version_str
+if os.path.exists(os.path.join(_mydir, ".git")):
+    git_hash = subprocess.check_output(
+        "git rev-parse --verify --short HEAD", cwd=_mydir, text=True, shell=True
+    ).strip()
+    git_version_str = f"v{version_str}"
+    tags = subprocess.check_output("git tag", cwd=_mydir, text=True, shell=True)
+    if git_version_str not in tags:
+        subprocess.check_output(
+            f"git tag -a {git_version_str} {git_hash} -m 'tagged by setup.py to {version_str}'",
+            cwd=_mydir,
+            text=True,
+            shell=True,
+        )
+    version_str = f"{version_str}+git.{git_hash}"
 
 setup(
     name="splatsetup",
-    version="1.0.0",
+    version=version_str,
     description="Utilities to write SPLAT .control files from a .toml template. Bokeh application for creating .control files",
     url="https://github.com/rocheseb/splat_setup",
     author="Sebastien Roche",
